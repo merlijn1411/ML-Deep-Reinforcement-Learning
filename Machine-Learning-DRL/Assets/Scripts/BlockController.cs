55b50bf087063c3461f9cded2fa1080f66c92853
merlijn1411 <114576658+merlijn1411@users.noreply.github.com>
2024-09-23 19:59:30 +0200

    update BlockController

--- Machine-Learning-DRL/Assets/Scripts/BlockController.cs
+++ Machine-Learning-DRL/Assets/Scripts/BlockController.cs
@@ -1,11 +1,10 @@
 using Unity.MLAgents;
-using Unity.MLAgents.Policies;
 using UnityEngine;
 
 public class BlockController : MonoBehaviour
 {
     [SerializeField] private float defaultBlockDistance = 2.0f;
-    private float _blockDistance;
+    private float blockDistance;
 
     [SerializeField] private GameObject seekerAgent;
     [SerializeField] private GameObject runnerAgent;
@@ -18,9 +17,12 @@ public class BlockController : MonoBehaviour
     public void BeginEpisodePosition()
     {
         // deze is verbonden met het Curriculum system in het yaml file.
-        _blockDistance = Academy.Instance.EnvironmentParameters.GetWithDefault("block_distance", defaultBlockDistance);
+        blockDistance = Academy.Instance.EnvironmentParameters.GetWithDefault("block_distance", defaultBlockDistance);
         
-        seekerAgent.transform.localPosition = new Vector3( -_blockDistance, 1f, -1);
-        runnerAgent.transform.localPosition = new Vector3(_blockDistance,1f,-1);
+        seekerAgent.transform.localPosition = new Vector3( -blockDistance, 1f, -1);
+        seekerAgent.transform.localRotation = new Quaternion();
+
+        runnerAgent.transform.localPosition = new Vector3(blockDistance,1f,-1);
+        runnerAgent.transform.localRotation = new Quaternion();
     }
 }
