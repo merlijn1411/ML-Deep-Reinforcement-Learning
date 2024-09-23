331e8821baa2eeeb5061c2c56b66a00a78939782
Manderijn <99815029+GaMerijn2@users.noreply.github.com>
2024-09-23 11:36:01 +0200

    scene

---
+++ Machine-Learning-DRL/results/ppo/run.bat
@@ -0,0 +1,5 @@
+cd C:\Users\merij\source\repos\ML-Deep-Reinforcement-Learning\Machine-Learning-DRL
+call C:\Users\merij\source\repos\ML-Deep-Reinforcement-Learning\Machine-Learning-DRL\venv\Scripts\activate
+
+
+pause
