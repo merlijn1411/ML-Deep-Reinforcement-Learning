9901b1d5f25f9b78f8baf0313be521ce1976680f
merlijn1411 <114576658+merlijn1411@users.noreply.github.com>
2024-09-30 10:04:15 +0200

    Added and changes
    Added:
    - Runner script
    - Runner Prefab
    Changes:
    - training environment prefabs
    - yaml file

---
+++ Machine-Learning-DRL/Assets/Scripts/RunnerAgent.cs
@@ -0,0 +1,16 @@
+using Unity.MLAgents;
+
+public class RunnerAgent : Agent
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
