3d1a22892d78afc0721f4671c5b7631bd44b4950
Merlijn <114576658+merlijn1411@users.noreply.github.com>
2024-10-08 10:33:24 +0200

    Feature/ml runner implementation (#3)
    * Added and changes
    
    Added:
    - Runner script
    - Runner Prefab
    Changes:
    - training environment prefabs
    - yaml file
    
    * removed all old training files
    
    * Update Prefabs and Agenst scripts and added tag
    
    * un used files removed
    
    * changes in pregabs and scene
    
    * changes in scene and gitignore
    
    * changes in scripts and pregabs and scene
    
    * Fixed bugs
    
    * changes in runner prefab and config file
    
    Runner:
      - Changing decision period from 5 to 15
    config file:
      - made two seprate TrainingArea one for seeker and one for runner
    
    * folder and config file
    
    - Folder Strcuture updated
    
    - config file updated
    
    * Curriculum worked
    
    Changes in:
    -SR_training_config.yaml
    -BlockController.cs
    
    * Update .gitignore
    
    * Create Info_Logs.py
    
    * Update .gitignore
    
    * Update yaml files and blockcontroller
    
    * update and add
    
    Update:
    - runner en seeker prefab.
    Add
    - Runner.onnx
    - Seeker.onnx
    
    * Files for in PPO
    
    Checkpoint and onnx files in ppo/Seeker & ppo/Runner
    
    * Update packages-lock.json
    
    * Update manifest.json
    
    * Update run_logs

---
+++ Machine-Learning-DRL/Assets/Scripts/Agents/SeekerAgent.cs
@@ -0,0 +1,139 @@
+using Unity.MLAgents;
+using Unity.MLAgents.Actuators;
+using Unity.MLAgents.Sensors;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class SeekerAgent : Agent
+{
+    [SerializeField] private Transform target;
+    [SerializeField] private float walkSpeed;
+    [SerializeField] private float rotationSpeed;
+    [SerializeField] private Timer countDown;
+    private float _previousDistance;
+    private float _distanceToTarget;
+
+    private Rigidbody _rBody;
+    
+    [SerializeField] private UnityEvent onNewEpisode;
+    
+    public override void Initialize()
+    {
+        _rBody = GetComponent<Rigidbody>();
+    }
+    public override void OnEpisodeBegin()
+    {
+        _rBody.velocity = Vector3.zero;
+        _rBody.angularVelocity = Vector3.zero;
+    }
+
+
+    //Deze method houd bij welke gegevens hij moet onthouden dat word gebruikt om een vedere beslissing te maken (dus action). 
+    public override void CollectObservations(VectorSensor sensor)
+    {
+        //Target en agent position
+        sensor.AddObservation(target.localPosition);
+        sensor.AddObservation(transform.localPosition);
+        
+        //Agent velocity
+        sensor.AddObservation(_rBody.velocity.x);
+        sensor.AddObservation(_rBody.velocity.z);
+    }
+
+    public override void OnActionReceived(ActionBuffers actionBuffers)
+    {
+        //Dit is inprencipe het zelfde als Input.GetAxis zodat de Machine kan leren bewegen.
+        var controlSignal = Vector3.zero;
+
+        var movement = actionBuffers.DiscreteActions[0];
+        var jump = actionBuffers.DiscreteActions[1];
+
+        if (movement == 1) { controlSignal.x = -1; }
+        if (movement == 2) { controlSignal.x = 1; }
+        if (movement == 3) { controlSignal.z = -1; }
+        if (movement == 4) { controlSignal.z = 1; }
+        
+        controlSignal.Normalize();
+        
+        transform.Translate(controlSignal * walkSpeed, Space.World);
+
+        FaceRotate(controlSignal);
+        _distanceToTarget = Vector3.Distance(transform.localPosition, target.localPosition);
+        
+        VelocityController();
+        
+        AgentFellOff();
+        SetReward(-0.001f);
+    }
+
+    private void FaceRotate(Vector3 controlSignal)
+    {
+        if (controlSignal == Vector3.zero) return;
+        var toRotationX = Quaternion.LookRotation(controlSignal, Vector3.up);
+    
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotationX, rotationSpeed);
+    }
+
+    public void TimerReachedZeroReward()
+    {
+        
+        var reward = (_distanceToTarget / 20f) * -1f; //de reward word op de hand van hoe dichtbij hij bij de target komt berekend.
+        SetReward(reward);
+        DistanceToTarget();
+        EndEpisode();
+        
+    }
+    private void DistanceToTarget()
+    {
+        // Reward for reducing distance to the cube
+        if (_previousDistance > _distanceToTarget)
+        {
+            AddReward(0.02f); // Give a small positive reward for getting closer
+        }
+        _previousDistance = _distanceToTarget;
+    }
+    
+    private void VelocityController()
+    {
+        if (_rBody.velocity.magnitude > 10f)
+            _rBody.velocity = Vector3.ClampMagnitude(_rBody.velocity, 10);
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag($"Runner"))
+        {
+            SetReward(1f);
+            onNewEpisode.Invoke();
+            EndEpisode();
+        }        
+        if (other.gameObject.CompareTag($"Wall"))
+        {
+            SetReward(-0.01f);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag($"RewardPoint"))
+        {
+            SetReward(0.3f);
+            Destroy(other.gameObject);
+        }
+    }
+
+    private void AgentFellOff()
+    {
+        if (!(transform.localPosition.y < 0)) return;
+        SetReward(-1f);
+        onNewEpisode.Invoke();
+        EndEpisode();
+    }
+    
+    // public override void Heuristic(in ActionBuffers actionsOut)
+    // {
+    //     var continuousActionsOut = actionsOut.ContinuousActions;
+    //     continuousActionsOut[0] = Mathf.Lerp(0,Input.GetAxis("Horizontal"),0.8f);
+    //     continuousActionsOut[1] = Mathf.Lerp(0,Input.GetAxis("Vertical"),0.8f);
+    // }
+}
--- Machine-Learning-DRL/Assets/Scripts/BlockController.cs
+++ Machine-Learning-DRL/Assets/Scripts/BlockController.cs
@@ -1,14 +1,17 @@
 using Unity.MLAgents;
-using Unity.MLAgents.Policies;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class BlockController : MonoBehaviour
 {
-    [SerializeField] private float defaultBlockDistance = 6.0f;
-    private float _blockDistance;
+    [SerializeField] private float timer;
+    [SerializeField] private float defaultBlockDistance;
 
     [SerializeField] private GameObject seekerAgent;
     [SerializeField] private GameObject runnerAgent;
+    
+    [SerializeField] private Timer countDown;
+    public UnityEvent onTimeReachesZero;
 
     private void Start()
     {
@@ -17,10 +20,22 @@ public class BlockController : MonoBehaviour
 
     public void BeginEpisodePosition()
     {
-        // deze is verbonden met het Curriculum system in het yaml file.
-        _blockDistance = Academy.Instance.EnvironmentParameters.GetWithDefault("block_distance", defaultBlockDistance);
+        countDown.t = timer; //reset de timer
         
-        seekerAgent.transform.localPosition = new Vector3( -_blockDistance, 1f, -1);
-        runnerAgent.transform.localPosition = new Vector3(_blockDistance,1f,-1);
+        // deze is verbonden met het Curriculum system in het yaml file.
+        var blockDistance = Academy.Instance.EnvironmentParameters.GetWithDefault("TrainingArea_Seeker", defaultBlockDistance);
+        
+        seekerAgent.transform.localPosition = new Vector3( -blockDistance, 1f, -1);
+        seekerAgent.transform.localRotation = new Quaternion();
+        
+        runnerAgent.transform.localPosition = new Vector3(blockDistance,1f,-1);
+        runnerAgent.transform.localRotation = new Quaternion();
+    }
+
+    private void Update()
+    {
+        if (!(countDown.t <= 0)) return;
+        BeginEpisodePosition();
+        onTimeReachesZero.Invoke();
     }
 }
